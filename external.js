06592924f8f52e81e7518338e7374044a6d5e0cb
LeoLee <lifenglsf@163.com>
2024-05-08 09:54:36 +0800

    Update external.js

--- external.js
+++ external.js
@@ -115,6 +115,7 @@
     if (localStorage.getItem("read")) {
       if(readCount>totalReadCount){
         console.log("read count比total read count大了，应该要停止了")
+        process.exit();
         return
       }
       if (
