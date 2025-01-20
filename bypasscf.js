d3961bf9a3110ad4fd5ce52b3fb820eeae8cd5ff
LeoLee <lifenglsf@163.com>
2025-01-20 18:12:26 +0800

    Update bypasscf.js

--- bypasscf.js
+++ bypasscf.js
@@ -53,7 +53,7 @@ const usernames = process.env.USERNAMES.split(",");
 const passwords = process.env.PASSWORDS.split(",");
 const loginUrl = process.env.WEBSITE || "https://linux.do"; //在GitHub action环境里它不能读取默认环境变量,只能在这里设置默认值
 const delayBetweenInstances = 10000;
-const totalAccounts =30; // 总的账号数
+const totalAccounts =usernames.length; // 总的账号数
 const delayBetweenBatches =
   runTimeLimitMillis / Math.ceil(totalAccounts / maxConcurrentAccounts);
 const isLikeSpecificUser = process.env.LIKE_SPECIFIC_USER || "false";
